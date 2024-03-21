88efd1517a95ff4adeb92870e41fe5744c7a1284
damyr-elmahdi <150464878+damyr-elmahdi@users.noreply.github.com>
2024-03-21 07:54:19 +0000

    Update Admin_spy.py
    it this one the  interface is good and works except for quantity

--- Admin_spy.py
+++ Admin_spy.py
@@ -15,7 +15,9 @@ from tkinter import ttk
 
 
 win1 = Tk()
-img1=ImageTk.PhotoImage(Image.open("images/pattern.png"))
+win1.title("HAVEN BOOK ")
+win1.iconbitmap('images/icon.ico')
+img1=ImageTk.PhotoImage(Image.open("images/background.png"))
 
 quantité={
             "book1": 11,
@@ -42,6 +44,7 @@ quantité={
 }
 
 def but_quantité():
+    
     win_qunt = Toplevel()
     win_qunt.geometry("950x370+200+280")
 
@@ -389,6 +392,7 @@ def but_quantité():
 
     win_qunt.mainloop() 
  
+
                    
 l1=customtkinter.CTkLabel(win1,image=img1)
 l1.pack()
@@ -396,7 +400,7 @@ l1.pack()
 frame=customtkinter.CTkFrame(master = l1, width=950, height=600, corner_radius=6)
 frame.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-imgage=ImageTk.PhotoImage(Image.open("images/interfav.png"))
+imgage=ImageTk.PhotoImage(Image.open("images/Haven.png"))
 lb=customtkinter.CTkLabel(frame,image=imgage)
 lb.pack()
 
@@ -436,7 +440,7 @@ def add():
         image_path = getattr(cover_button, 'file_path', None)
         price = price_entry.get()
         if image_path and price:
-            # Calculate x and y based on the number of entries in the CSV
+           
             x, y = calculate_coordinates('books.csv')
             with open('books.csv', mode='a', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
@@ -447,13 +451,13 @@ def add():
             with open(csv_filename, mode='r', newline='', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 entries = list(reader)
-                # Calculate the number of rows already present
-                num_rows = len(entries) - 1  # Subtract 1 for the header
-                x = (num_rows % 5) * 230 + 20  # Change 5 to how many books per row you want
+                
+                num_rows = len(entries) - 1  
+                x = (num_rows % 5) * 230 + 20  
                 y = (num_rows // 5) * 230 + 100
                 return x, y
         except FileNotFoundError:
-            # If the file doesn't exist, start from the first position
+            
             return 20, 100
         
         with open('books.csv', mode='a', newline='', encoding='utf-8') as file:
@@ -494,9 +498,25 @@ Books = {
 
 
 booksdesc = {
-    "book1": "Darkest before the dawn is a gripping tale of resilience, hope, and redemption in the face of adversity.",
-    "book2": "Treasure Island is a classic adventure novel written by Robert Louis Stevenson, first published as a book in 1883.",
-    "book3": "Magic of the Unicorn is a captivating fantasy novel that transports readers into a world filled with wonder, adventure, and enchantment.",
+"book1": "Darkest before the dawn is a gripping tale \nof resilience, hope, and redemption in the face \nof adversity. Written by an acclaimed author, \nit follows the journey of several interconnected characters as \nthey navigate through life's trials and tribulations.\nSet against the backdrop of a small, struggling \ntown, the narrative delves into the lives of \nits inhabitants, each grappling with \ntheir own personal demons. \nFrom a troubled teenager battling addiction to a \ndisillusioned war veteran haunted by his past, \nthe characters' stories intertwine in unexpected ways, \nultimately converging towards a powerful climax.",
+"book2": "Darkest before the dawn is a gripping tale \nof resilience, hope, and redemption in the face \nof adversity. Written by an acclaimed author, \nit follows the journey of several interconnected characters as \nthey navigate through life's trials and tribulations.\nSet against the backdrop of a small, struggling \ntown, the narrative delves into the lives of \nits inhabitants, each grappling with \ntheir own personal demons. \nFrom a troubled teenager battling addiction to a \ndisillusioned war veteran haunted by his past, \nthe characters' stories intertwine in unexpected ways, \nultimately converging towards a powerful climax.",
+"book3": "Magic of the Unicorn is a captivating \nfantasy novel that transports readers into \na world filled with wonder, adventure, \nand enchantment. Written by an imaginative \nauthor, it tells the tale of a young protagonist \nwho discovers a realm where unicorns, mystical \ncreatures of legend, exist.The story follows \nthe journey of a courageous protagonist, often \na child or teenager, who stumbles upon a \nhidden portal or magical artifact that \ngrants them access to the realm of unicorns. \nOnce there, they encounter breathtaking landscapes, \nancient forests, and shimmering meadows inhabited \nby these elusive creatures.",
+"book4": "Summer Adventure Stories is a collection of \nexhilarating tales crafted to ignite the spirit \nof adventure in readers of all ages. \nWithin its pages, readers embark on thrilling \njourneys to distant lands, embark on daring quests, \nand experience the magic of summertime\n escapades. Each story in the collection offers \na unique adventure, from exploring hidden treasure \ntroves in tropical islands to embarking on epic \nhikes through rugged mountain ranges. \nThe characters range from intrepid \nexplorers to curious children, \neach facing challenges and overcoming \nobstacles with bravery and determination.",
+"book5": "Dragon Run is an enthralling young adult fantasy \nnovel that takes readers on an epic journey \nthrough a world filled with dragons, \nmagic, and adventure. Written by an acclaimed author, \nit follows the thrilling quest of a young \nprotagonist who must navigate a perilous landscape \nto save their kingdom from an ancient evil.\n The story unfolds in a land where dragons \nonce roamed freely, but have since been hunted \nto near extinction. The protagonist, often a \ncourageous and resourceful youth, discovers \nthat they have a special connection to these mythical \ncreatures, which sets them on a path of \ndiscovery and destiny.",
+"book6": "Abominable Snowman is a gripping adventure novel that \nexplores the legend of the mythical creature known as the \nYeti or the Abominable Snowman. Written by a renowned \nauthor, it follows a group of explorers and scientists as they \nembark on a daring expedition into the remote and \ntreacherous Himalayan mountains in search of the \nelusive creature. The story unfolds as \nthe expedition team faces the harsh and unforgiving \nconditions of the Himalayas, battling against extreme cold, \nrugged terrain, and the ever-present danger of avalanches \nand crevasses. As they delve deeper into the wilderness, \ntensions rise within the group as they grapple with their \nown fears and doubts, while also confronting the mysteries \nsurrounding the existence of the legendary creature.",
+"book7": "The Chronicles of Narnia is a beloved fantasy \nseries written by C.S. Lewis that takes readers on \nan extraordinary journey through the magical \nland of Narnia. Comprising  seven books,the \nseries weaves together tales of \nadventure, courage, and redemption, \ncaptivating readers of all ages with its \nrichly imagined world and timeless themes. \nAt the heart of the series is the magical \nwardrobe, which serves as a portal \nto Narnia, a realm inhabited by talking \nanimals, mythical creatures, and powerful \nsorcery. Each book follows the adventures \nof different characters who discover Narnia \nand play pivotal roles in its history.",
+"book8": "Underwater is a gripping contemporary novel \nthat delves into the complexities of \nhuman emotions and resilience in the face \nof adversity. Written by an acclaimed author, \nit follows the journey of a young protagonist \nas she navigates the aftermath of a traumatic \nevent and seeks to find healing and \nredemption. The story centers around \nthe protagonist, often a teenage girl or \nyoung woman, who finds herself struggling to \ncope with the aftermath of a devastating \nincident, such as a natural disaster, a personal loss, or a \ntraumatic experience. As she grapples with feelings of \nguilt, fear, and isolation, she embarks on a journey.",
+"book9": "As If It Were a Movie is a thought-provoking \ncontemporary novel that delves into the complexities \nof identity, relationships, and the blurred lines between \nreality and fiction. Written by a talented \nauthor, it offers a unique narrative structure \nthat challenges traditional storytelling conventions. \nThe story follows the protagonist, often a \nyoung adult or teenager, who finds themselves grappling \nwith the challenges of growing up \nand navigating the complexities of modern \nlife. As they navigate relationships, school, and \nthe pursuit of their dreams, they also find themselves \ndrawn into a world of fantasy and imagination.",
+"book10": "Takings is a gripping legal thriller that follows the \njourney of a determined attorney as she takes on a high\n-stakes case that could change the course of her career \nand impact the lives of countless individuals. \nSet in the heart of a bustling city, the novel explores \nthemes of justice, power, and the ethical dilemmas faced \nby those in the legal profession. The story centers around \nSarah Williams, a brilliant and ambitious young lawyer who \nfinds herself thrust into the spotlight when she is \nassigned to represent a group of homeowners facing \neminent domain proceedings. As a powerful corporation \nseeks to acquire their properties for a lucrative development \nproject, Sarah must navigate a web of legal challenges, \npolitical intrigue, and personal conflicts.",
+"book11": "Solar Bones is a mesmerizing and thought-\nprovoking novel that unfolds as a single, \ncontinuous stream of consciousness. Written by \nacclaimed Irish author Mike McCormack, it offers \na unique and immersive reading experience that challenges \ntraditional narrative structures. The story centers \naround Marcus Conway, an engineer living in a small town \nin Ireland. As Marcus reflects on his life, memories, \nand experiences, the novel takes readers on a journey \nthrough his thoughts, emotions, and innermost reflections. \nFrom his childhood memories to his struggles with work, \nfamily, and mortality, Marcus's narrative offers a poignant \nand deeply personal exploration of the human condition.",
+"book12": "Death Sentence is a gripping thriller that plunges \nreaders into a world of danger, intrigue, and moral \nambiguity. Written by a master of the genre, it follows \nthe pulse-pounding journey of a protagonist \nwho finds themselves caught \nin a deadly game of cat and mouse with a ruthless \nadversary. The story centers around John Smith, \nan ordinary man whose life is turned upside down \nwhen he receives a mysterious \npackage containing evidence of a crime he \ndidn't commit. As he delves deeper into the mystery, \nJohn discovers that he has become the target of a \npowerful and shadowy organization determined \nto silence him at any cost",
+"book13": "Heart Few is a tender and poignant love story that \nunfolds against the backdrop of a quaint coastal town. \nWritten by an acclaimed romance author, it follows the \nintertwined lives of two individuals whose paths \nunexpectedly cross, leading to a transformative journey \nof love, healing, and self-discovery. The story centers \naround Emily, a spirited young woman who has always \nharbored dreams of adventure and exploration, and Jack, a \nreserved yet compassionate artist struggling to overcome \nhis own past traumas. When Emily returns to her hometown \nto care for her ailing grandmother, she never expects \nto meet Jack, whose quiet strength and artistic soul \ncaptivate her from the moment they meet.",
+"book14": "The Dividing Line is a gripping psychological thriller \nthat delves into the darkest corners of the human \nmind and the blurred boundaries between truth and \ndeception. Written by a master storyteller, it weaves \na tale of suspense, intrigue, and moral ambiguity that \nkeeps readers on the edge of their seats until the very \nend. Set in a small town with its own secrets and \nscandals, the story follows the lives of several \ninterconnected characters whose fates become \nintertwined by a series of shocking events. \nAt the center of the narrative is a mysterious \ndividing line that separates the town into two distinct \nworlds, each with its own set of \nrules and expectations.",
+"book15": "The Billion Dollar Spy is a riveting non-fiction \nnarrative that unveils the gripping true story of \nespionage during the Cold War era. \nAuthored by PulitzerPrize-nominated journalist \nDavid E. Hoffman, this meticulously researched \nbook sheds light on one of the most significant \nintelligence operations of the 20th century. \nSet against the backdrop \nof the Cold War tensions between the United \nStates and the Soviet Union, the book follows the clandestine \nactivities of Adolf Tolkachev, a Soviet engineer who \nbecomes a top spy for the CIA. Tolkachev, driven by \ndisillusionment with the oppressive Soviet regime, risks \nhis life to provide crucial intelligence to the \nAmerican intelligence agency.",
+"book16": "The House on the Lake is a gripping mystery novel \nthat unfolds gainst the backdrop of a secluded and \npicturesque lakeside retreat. Written by a master \nof suspense, it weaves a tale of secrets, betrayal, \nand redemption that keeps readers guessing until \nthe very end. The story follows the protagonist, often \na troubled individual seeking solace and a fresh start, \nwho discovers an idyllic house nestled by the tranquil \nwaters of a remote lake. Drawn by the promise of peace and \nsolitude, they soon find themselves entangled in a web of \nmystery and intrigue as they uncover the dark \nsecrets hidden within the walls of the house.",
+"book17": "Mount Majestic is a captivating adventure novel that \ntransports readers to a magical island filled with mystery, \ndanger, and discovery. Written by an imaginative author, \nit follows the thrilling journey of a young protagonist \nas they embark on a quest to unravel \nthe secrets of a legendary mountain. Set on the \nenchanting island of Mount Majestic, \nthe story centers around a spirited protagonist, \noften a courageous child or teenager, who dreams \nof adventure and exploration. When strange occurrences \nbegin to disrupt the peace of the island, \nthe protagonist sets out on a daring quest to uncover \nthe truth behind the mysterious events.",
+"book18": "Joe Hill is a riveting psychological thriller that \nexplores the dark and twisted depths of the human psyche. \nWritten by an acclaimed author known for their mastery \nof suspense, this gripping novel follows \nthe tumultuous journey of Joe Hill, a seemingly ordinary \nman whose life takes a sinister turn when \nhe becomes entangled in a web of deception and \nbetrayal. \nSet against the backdrop of a small, close-knit \ncommunity, the story begins innocently \nenough, with Joe Hill leading a quiet and unassuming \nexistence. But when a series of shocking events unfolds, \nJoe finds himself thrust into a world of \ndanger and intrigue, where nothing is as it \nseems and trust is a luxury he can ill afford.",
+
 }
     
 def modify_info():
@@ -542,25 +562,25 @@ def modify_info():
       
         selected_book = book_combo.get()
         new_price = price_entry.get()
-        # Update the book's price if the input is valid
+   
         if new_price.isdigit():
-            # Read all data from the CSV file
+            
             books_data = []
             with open('book_pr.csv', mode='r', newline='', encoding='utf-8') as file:
                 reader = csv.reader(file)
                 for row in reader:
                     if row[0] == selected_book:
-                        # Update the price of the selected book
+               
                         books_data.append([selected_book, new_price])
                     else:
                         books_data.append(row)
             
-            # Write the updated data back to the CSV file
+            
             with open('book_pr.csv', mode='w', newline='', encoding='utf-8') as file:
                 writer = csv.writer(file)
                 writer.writerows(books_data)
 
-    confirm_button = ctk.CTkButton(modify_info_window, text="Confirm", font=('Helvetica', 20), height=60, width=200, command=update_price and update_desc)
+    confirm_button = ctk.CTkButton(modify_info_window, text="Confirm", font=('Helvetica', 20), height=60, width=200, command=lambda: [update_price(), update_desc()])
     confirm_button.pack(pady=25)
 
 
@@ -569,19 +589,23 @@ def modify_info():
 def delete_use():
     
     import delet_user
+    
+    
 
-Add_b = customtkinter.CTkButton(win1, text="Add book ", font=('Avantstile Regular', 20),text_color="black", fg_color="blue", command=add)
+    
+
+Add_b = customtkinter.CTkButton(win1, text="Add book ", font=('Avantstile Regular', 20),text_color="#EADCCE", corner_radius=30 ,bg_color="#634937", fg_color="#B68B73", command=add)
 
 Add_b.place(x=385, y=355) 
 
 
-quantité = customtkinter.CTkButton(frame, text="Quantité ", font=('Times New Roman',20),text_color="black",fg_color="green",command=but_quantité)
+quantité = customtkinter.CTkButton(frame, text="Quantité ", font=('Times New Roman',20),text_color="#EADCCE", corner_radius=30 ,bg_color="#634937",fg_color="#B68B73",command=but_quantité)
 quantité.place(x=100, y=455) 
 
-mod = customtkinter.CTkButton(win1, text="Modify book ",font=('Avantstile Regular', 20),text_color="black", fg_color="light green", command=modify_info)
+mod = customtkinter.CTkButton(win1, text="Modify book ",font=('Avantstile Regular', 20),text_color="#EADCCE", corner_radius=30 ,bg_color="#634937", fg_color="#B68B73", command=modify_info)
 mod.place(x=800, y=355) 
 
-delet = customtkinter.CTkButton(win1, text="Delet user ",font=('Avantstile Regular', 20),text_color="black",fg_color="red", command=delete_use)
+delet = customtkinter.CTkButton(win1, text="Delet user ",font=('Avantstile Regular', 20),text_color="#EADCCE", corner_radius=30,bg_color="#634937",fg_color="#B68B73", command=delete_use)
 delet.place(x=800, y=555)                 
 verify_information()
 
